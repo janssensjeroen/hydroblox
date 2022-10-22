6695615c322a3cc254cd53652a50ed32b34c6f4d
Jeroen Janssens <jeroen.janssens@accentjobs.be>
2022-10-22 10:48:46 +0200

    goerli network

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -1,3 +1,7 @@
+const { mnemonic, infura_apikey } = require('./secrets.json');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const goerli_uri = `wss://goerli.infura.io/ws/v3/${infura_apikey}`
+
 module.exports = {
   compilers: {
     solc: {
@@ -5,10 +9,24 @@ module.exports = {
     }
   },
   networks: {
-    localnode: {
+    development: {
       network_id: "*",
       host: "localhost",
-      port: 7545,
-    }
+      port: 8545,
+    },
+    goerli: {
+      provider: () => new HDWalletProvider(mnemonic, goerli_uri),
+      network_id: '5',
+      websocket: true,
+      // gas: 90000000,
+      // gasPrice: 10000000000,
+      // confirmations: 2,
+      // timeoutBlocks: 200,
+      gas: 4500000,
+      gasPrice: 10000000,
+      skipDryRun: true,
+      networkCheckTimeout: 10000000,
+      setTimeout: 5000000000
+    },
   }
 };
